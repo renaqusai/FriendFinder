69e227b310cdefbf7cdc04c74101fb2c09760b3a
Requ Amer <requamer@Requs-MacBook-Pro.local>
2018-12-21 23:16:55 -0800

    homework10

---
+++ app/data/friend.js
@@ -0,0 +1,84 @@
+var friends = [
+	{
+		name: 'Bob',
+		pic: 'http://i0.kym-cdn.com/entries/icons/original/000/014/700/how-to-draw-bob-bobs-burgers_1_000000007041_5.jpg',
+		surveyResults: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',			
+		]
+	},
+	{
+		name: 'Cartman',
+		pic: 'https://consequenceofsound.files.wordpress.com/2017/08/gkhjzfp.jpg?quality=80&w=807',
+		surveyResults: [
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+		]
+	},
+	{
+		name: 'Morty',
+		pic: 'https://i.dgif.co/gifs/rm49/O/rm49.gif',
+		surveyResults: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',			
+		]
+	},
+	{
+		name: 'Uncle Rick',
+		pic: 'https://images.moviepilot.com/images/c_limit,q_auto:good,w_600/vdc49xfybuuua6fd2jjx/everything-we-know-so-far-about-rick-and-morty-season-3.jpg',
+		surveyResults: [
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',			
+		]
+	},
+	{
+		name: 'Saurai Jack',
+		pic: 'http://nerdist.com/wp-content/uploads/2016/07/Samurai-Jack-2016.jpg',
+		surveyResults: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',			
+		]
+	}
+];
+
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,46 @@
+// Your apiRoutes.js file should contain two routes:
+// 	A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+// 	A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+var friends = require("../data/friends.js");
+
+module.exports = function (app) {
+
+	app.get("/api/friends", function (req, res) {
+		res.json(friends);
+		console.log("Number of entries: ",friends.length);
+	});
+
+	app.post("/api/friends", function (req, res) {
+
+		var bff = {
+			name: "nil",
+			pic: "nil",
+			friendDifference: 1000
+		};
+
+		var userProfile = req.body;
+		var usersScores = userProfile.surveyResults;
+		var totalDiff = 0;
+
+		for (var i = 0; i < friends.length; i++) {
+			totalDiff = 0;
+
+			for (var j = 0; j < friends[i].surveyResults.length; j++) {
+				
+				totalDiff += Math.abs(parseInt(usersScores[j]) - parseInt(friends[i].surveyResults[j]));
+
+				if (totalDiff <= bff.friendDifference) {
+					bff.name = friends[i].name;
+					bff.pic = friends[i].pic;
+					bff.friendDifference = totalDiff;
+				}
+			}
+		}
+
+		friends.push(userProfile);
+
+		res.json(bff); 
+
+	});
+}
